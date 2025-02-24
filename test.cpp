fe40dcc370bf86d7b7d3fb383e19b851eb46a59f
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-02-24 23:30:53 +0530

    How to print data in C++

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+// using namespace std;  if this line is not import then in line 6 do  syntax
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl; // std:: is use of not include line 2
 }
